51a207429548299d9326b73e20e3ad60d8c78122
sofia-bjorkesjo <sb223fz@student.lnu.se>
2017-03-29 14:08:33 +0200

    I have done some changes

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ let app = express();
 
 let port = process.env.PORT || 3000;
 
+app.engine('.hbs', exphbs({
+    defaultLayout: 'default',
+    extname: 'hbs'
+}));
+
+app.set('view engine', 'hbs');
+
+app.use('/', require('./routes/home'));
+
 app.listen(port, function () {
     console.log('Express started on http://localhost' + port);
 });
---
+++ routes/home.js
@@ -0,0 +1,8 @@
+'use strict';
+
+let router = require('express').Router();
+
+router.route('/'))
+    .get(function (req, res) {
+        res.render(('home/index'));
+});
