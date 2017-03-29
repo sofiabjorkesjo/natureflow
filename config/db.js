88e3a67816a9d8acbad48b2d2acbeb8d6d23d3a9
sofia-bjorkesjo <sb223fz@student.lnu.se>
2017-03-29 14:33:16 +0200

    I have connected to mongoose

--- config/db.js
+++ config/db.js
@@ -1,5 +1,29 @@
 'use strict';
+
+let mongoose = require('mongoose');
+
 module.exports = function () {
     let link = 'mongodb://sofiasuser:sofia@ds145380.mlab.com:45380/sofiasdatabas';
-    let db =
-}
+    let db =mongoose.connect(link);
+
+    db.connection.on('Connected', function () {
+        console.log('Successfully connected to db');
+    });
+
+    db.connection.on('error', function () {
+        console.log('We got a connection error');
+    });
+
+    db.connection.on('disconnected', function () {
+        console.log('Mongoose connection disconnected');
+    });
+
+    process.on('SIGNT', function () {
+        db.connection.close(function () {
+            console.log('Mongoose connection disconnected through app termination');
+            process.exit(0);
+        });
+    });
+
+    return db;
+};
