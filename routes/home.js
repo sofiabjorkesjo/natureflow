8639330fbf25b690e732712da378f92b589c3be9
sofia-bjorkesjo <sb223fz@student.lnu.se>
2017-03-30 14:03:31 +0200

    I can sign up

--- routes/home.js
+++ routes/home.js
@@ -1,6 +1,7 @@
 'use strict';
 
 let router = require('express').Router();
+let User = require('../models/User');
 
 router.route('/')
     .get(function (req, res) {
@@ -10,6 +11,33 @@ router.route('/')
 router.route('/sign-up')
     .get(function (req, res) {
             res.render('basic/sign_up');
+    })
+    .post(function (req, res) {
+            let userObject = new User({
+                    username: req.body.username,
+                    password: req.body.password
+            });
+
+            User.findOne({username: req.body.username}).then(function (data) {
+                    if (data) {
+                            console.log('user finns redan');
+                    } else {
+                            userObject.save()
+                                .then(function () {
+                                        console.log(userObject);
+                                        res.redirect('/')
+                                })
+                                .catch(function (err) {
+                                        if (err) {
+                                                console.log(err);
+                                        }
+                                        res.redirect('/sign-up');
+                                });
+                    }
+            });
+
     });
+
+
 
 module.exports = router;
