3a875aa00cb60a276ca6a24abedce2c42c0a3b26
sofia-bjorkesjo <sb223fz@student.lnu.se>
2017-03-31 11:25:25 +0200

    I have done some changes

--- public/client.js
+++ public/client.js
@@ -26,3 +26,24 @@ imgDiv.appendChild(buttonRight);
 wrapper.appendChild(imgDiv)
 
 let slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    let i;
+    let imgSlides = document.getElementsByClassName('imgSlide')
+    if (n > x.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = x.length;
+    }
+
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = 'none';
+    }
+    x[slideIndex-1].style.display = 'block';
+}
