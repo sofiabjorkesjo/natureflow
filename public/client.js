490c4d33d1ae5bff75d81bc4aceccac1b0e35e7a
sofia-bjorkesjo <sb223fz@student.lnu.se>
2017-04-03 09:35:17 +0200

    Now I have a image slide on index

--- public/client.js
+++ public/client.js
@@ -16,6 +16,7 @@ let buttonRight = document.createElement('button');
 buttonRight.setAttribute('class', 'buttonRight');
 
 let imgDiv = document.createElement('div');
+imgDiv.setAttribute('id', 'imgDiv');
 
 let wrapper = document.getElementById('wrapper');
 imgDiv.appendChild(img);
@@ -23,27 +24,41 @@ imgDiv.appendChild(img2);
 imgDiv.appendChild(img3);
 imgDiv.appendChild(buttonLeft);
 imgDiv.appendChild(buttonRight);
-wrapper.appendChild(imgDiv)
+wrapper.appendChild(imgDiv);
 
+buttonLeft.addEventListener('click', function () {
+    plusDivs(-1);
+});
+
+buttonRight.addEventListener('click', function () {
+    plusDivs(1);
+});
+
+//första bilden
 let slideIndex = 1;
 showDivs(slideIndex);
 
+//hämtar nästa bild, antingen framåt eller bakåt beroende på vilken knapp man trycker på
 function plusDivs(n) {
     showDivs(slideIndex += n);
 }
 
-function showDivs(n) {
+//gömmer alla bilderna och sätter display till block ovh visar en bild med det satta slideindex.
+//Om slideIndex är högre än numret av element så sätts de till noll.
+//Om slideIndex är mindre än 1 så sätts den till numret av element imgSlide.length.
+
+function showDivs(x) {
     let i;
-    let imgSlides = document.getElementsByClassName('imgSlide')
-    if (n > x.length) {
+    let imgSlide = document.getElementsByClassName('imgSlide')
+    if (x > imgSlide.length) {
         slideIndex = 1;
     }
-    if (n < 1) {
-        slideIndex = x.length;
+    if (x < 1) {
+        slideIndex = imgSlide.length;
     }
 
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = 'none';
+    for (i = 0; i < imgSlide.length; i++) {
+        imgSlide[i].style.display = 'none';
     }
-    x[slideIndex-1].style.display = 'block';
+    imgSlide[slideIndex-1].style.display = 'block';
 }
