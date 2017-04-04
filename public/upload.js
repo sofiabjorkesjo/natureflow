09ad1b010d937cdb13ec761f499d9b3cab7980e8
sofia-bjorkesjo <sb223fz@student.lnu.se>
2017-04-04 12:38:16 +0200

    I can upload a picture

--- public/upload.js
+++ public/upload.js
@@ -1,3 +1,38 @@
-'use strict';
 
-document.getElementById('uploadPicture').addEventListener('change', readURL, true);
+
+
+
+window.onload = function () {
+
+    let test = document.createElement('p');
+    test.textContent = 'hejsan';
+    let wrapper = document.getElementById('wrapper');
+    wrapper.appendChild(test);
+
+
+
+    let uploadPicture = document.getElementById('uploadPicture');
+    let addPicture = document.getElementById('addPicture');
+    
+    uploadPicture.addEventListener('change', function () {
+        let file = uploadPicture.files[0];
+        let imageType = /image.*/;
+
+        if (file.type.match(imageType)) {
+            let reader = new FileReader();
+
+            reader.onload = function (e) {
+                addPicture.innerHTML = "";
+
+                let img = new Image();
+                img.src = reader.result;
+
+                addPicture.appendChild(img);
+            };
+
+            reader.readAsDataURL(file);
+        } else {
+            addPicture.innerHTML = "File not supported";
+        }
+    });
+}
