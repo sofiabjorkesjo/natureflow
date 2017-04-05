ceac2b661f1e841b16c4c0580908e4282c5529ac
sofia-bjorkesjo <sb223fz@student.lnu.se>
2017-04-05 16:49:34 +0200

    I have done some changes

--- public/upload.js
+++ public/upload.js
@@ -1,9 +1,13 @@
 
 
+//väljer en fil som är av typen image. Om bilden man valt matchar imageType så skapas en ny FileReader som läser in filen.
+//En ny bild skapas och src sätts till resultatet som lästs in från FIleReader.
+//Bilden appendas till divtaggen.
 
 
 window.onload = function () {
 
+    let wrapper = document.querySelector('#wrapper');
     let uploadPicture = document.getElementById('uploadPicture');
     let imageDiv = document.getElementById('imageDiv');
     
@@ -21,6 +25,20 @@ window.onload = function () {
                 img.src = reader.result;
 
                 imageDiv.appendChild(img);
+
+                let uploadButton = document.createElement('input');
+                uploadButton.setAttribute('type', 'submit');
+                uploadButton.setAttribute('value', 'upload');
+                wrapper.appendChild(uploadButton);
+
+                //if satsen funkar ej - fixa
+                if (uploadButton = true) {
+                    console.log('tjotjo')
+                   wrapper.removeChild(wrapper.childNodes[0]);
+                } else {
+                    wrapper.appendChild(uploadButton);
+                }
+
             };
 
             reader.readAsDataURL(file);
