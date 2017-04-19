ffc6d7194b2e5e539d2bc7d73ca9ef957c4964ae
sofia-bjorkesjo <sb223fz@student.lnu.se>
2017-04-19 15:20:12 +0200

    I have done some css changes

--- public/comment.js
+++ public/comment.js
@@ -1,18 +1,18 @@
 'use strict';
 
 let modal = document.getElementById('myModal');
-let btn = document.getElementsByClassName('addComment');
+let btn = document.querySelectorAll('.addComment');
 console.log(btn.length);
 let span = document.getElementsByClassName('close')[0];
 
 
 for (let i = 0; i < btn.length; i++) {
+    btn[i].onclick = function () {
+        modal.style.display = 'block';
+    };
 
 }
 
-btn.onclick = function () {
-    modal.style.display = 'block';
-};
 
 span.onclick = function () {
     modal.style.display = 'none';
