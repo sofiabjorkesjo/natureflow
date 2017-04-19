6bab6e25b5bce763bccb1647e3649bb0382f3605
sofia-bjorkesjo <sb223fz@student.lnu.se>
2017-04-19 12:44:29 +0200

    I have done a box to my comments

---
+++ public/comment.js
@@ -0,0 +1,19 @@
+'use strict';
+
+let modal = document.getElementById('myModal');
+let btn = document.getElementById('addComment');
+let span = document.getElementsByClassName('close')[0];
+
+btn.onclick = function () {
+    modal.style.display = 'block';
+};
+
+span.onclick = function () {
+    modal.style.display = 'none';
+};
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+};
