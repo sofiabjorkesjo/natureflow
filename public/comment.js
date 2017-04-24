21529e210d8e9de99045435ff554312cc99560b4
sofia-bjorkesjo <sb223fz@student.lnu.se>
2017-04-24 13:43:37 +0200

    my comments works

--- public/comment.js
+++ public/comment.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let modal = document.getElementsByClassName('modal');
+let modell = document.getElementsByClassName('model');
 let btn = document.querySelectorAll('.addComment');
 console.log(btn.length);
 let span = document.getElementsByClassName('close');
@@ -10,24 +10,42 @@ let nrOfImages = btn.length;
 
 for (let i = 0; i < nrOfImages; i++) {
     btn[i].addEventListener('click', function () {
-        modal[i].style.display = 'block';
+        modell[i].style.display = 'block';
     });
     span[i].addEventListener('click', function () {
-        modal[i].style.display = 'none';
+        modell[i].style.display = 'none';
     });
 }
 
 window.addEventListener('click', function () {
     for (let j = 0; j < nrOfImages; j++) {
-        if (event.target === modal[j]) {
-            modal[j].style.display = 'none';
+        if (event.target === modell[j]) {
+            modell[j].style.display = 'none';
         }
     }
 
 });
 
-
-
 //Klickar upp boxen för att skriva kommentar, och klicka ner boxen
 
+let modellComments = document.getElementsByClassName('modelComments');
+let button = document.querySelectorAll('.comments');
+let closeButton = document.getElementsByClassName('closeComments');
+
+for (let i = 0; i < button.length; i ++) {
+    button[i].addEventListener('click', function () {
+        modellComments[i].style.display = 'block';
+    });
+    closeButton[i].addEventListener('click', function () {
+        modellComments[i].style.display = 'none';
+    });
+}
+
+window.addEventListener('click', function () {
+    for (let i = 0; i < button.length; i++) {
+        if (event.target === modellComments[i]) {
+            modellComments[i].style.display = 'none';
+        }
+    }
+});
 
