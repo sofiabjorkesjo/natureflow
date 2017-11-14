660f6af92de7a3962ccbb8b800f06ae9d770d855
sofia-bjorkesjo <sb223fz@student.lnu.se>
2017-11-14 15:43:42 +0100

    changed port

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(fileUpload());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-let port = process.env.PORT || 3000;
+let port = process.env.PORT || 3001;
 
 //connectar till mongoose
 mongoose();
